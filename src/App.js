ae716023c74392d56d217885421b0c8aecf29160
Richa-Rastogi25 <richa.rastogi234@gmail.com>
2025-12-23 17:08:29 +0530

    Continuous Deployment of Game Website using CI/CD Pipeline through my S3 Bucket

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     setShowNextTurn(true);
     setTimeout(() => {
       setShowNextTurn(false);
-      setTurn(prev => prev + 1);
-    }, 5000); // text shown for 1 second
+      setTurn(prev => prev + 0);
+    }, 8000); // text shown for 8 second
   }
 
   return (
